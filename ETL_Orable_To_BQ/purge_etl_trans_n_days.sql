09a5218dd797593caebfa2d431d7baf0560aefc8
technqvi <technqvi_gangster@hotmail.com>
2024-01-27 16:11:43 +0700

    นา

--- ETL_Orable_To_BQ/purge_etl_trans_n_days.sql
+++ ETL_Orable_To_BQ/purge_etl_trans_n_days.sql
@@ -1,2 +1,2 @@
-DELETE from log_error where error_datetime<=(select DATE('now', '-45 day'));
-DELETE from etl_transaction where etl_datetime<=(select DATE('now', '-45 day'));
+DELETE from log_error where error_datetime<=(select DATE('now', '-31 day'));
+DELETE from etl_transaction where etl_datetime<=(select DATE('now', '-31 day'));
